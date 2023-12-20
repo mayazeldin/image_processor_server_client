4b96caad2fccdc44621efe30c35b7ba0a462ebdf
mayazeldin <zeldin.m@northeastern.edu>
2023-12-20 13:49:35 -0500

    Changed server script

--- setup.sh
+++ setup.sh
@@ -5,8 +5,8 @@ if command -v python3 &>/dev/null; then
     echo "Python 3 is already installed."
 else
     # Update the package list and install Python 3
-    sudo apt-get update
-    sudo apt-get install -y python3
+    apt-get update
+    apt-get install -y python3
     echo "Python 3 has been installed."
 fi
 
@@ -16,7 +16,7 @@ if command -v pip3 &>/dev/null; then
     echo "pip is already installed."
 else
     # Install pip for Python 3
-    sudo apt-get install -y python3-pip
+    apt-get install -y python3-pip
     echo "pip has been installed."
 fi
 
@@ -28,7 +28,7 @@ if command -v git &>/dev/null; then
     echo "git is already installed."
 else
     # Install git
-    sudo apt-get install -y git
+    apt-get install -y git
     echo "git has been installed."
 fi
 
